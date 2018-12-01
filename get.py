75a88b6f6d58b51bd40172fe29c60d98f1a0dba2
Vivek Verma <vivnps.verma@gmail.com>
2018-12-01 13:06:22 -0800

    Adding NLP

--- get.py
+++ get.py
@@ -1,5 +1,6 @@
 from newsapi import NewsApiClient
 from functions import *
+from newspaper import Article
 
 API_KEY = "2cce71b97f664724b621d4e37e113009"
 
@@ -8,11 +9,15 @@ def get_news(topic):
     news_api = NewsApiClient(api_key=API_KEY)
     top_headlines = news_api.get_top_headlines(q=topic, language='en')
     articles = top_headlines['articles']
-    top = []
-    for i in range(0,5):
-        top.append([articles[i]['title']])
-
-    l_print(top)
+    title = []
+    url = []
+    for i in range(0, 5):
+        title.append(articles[i]['title'])
+        url.append(articles[i]['url'])
+    article = Article(url[0])
+    article.download()
+    article.parse()
+    print(article.text)
 
 
 if __name__ == '__main__':
